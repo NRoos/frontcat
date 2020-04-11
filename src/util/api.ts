b7bb80e52d982b878cdfb4a12ee36617bb8964be
Nico Roos <nico.roos@perfektio.fi>
2020-04-11 06:37:02 +0300

    Refactored to actually use the graphql endpoints

--- src/util/api.ts
+++ src/util/api.ts
@@ -1,15 +1,31 @@
 const url = process.env.REACT_APP_URL || 'http://localhost:8001';
 
-const getBreeds = async (callback: Function) => {
- return fetch(`${url}/breeds`)
-   .then((res) => res.json())
-   .then((data) => callback(data))
+const options = {
+  method: 'POST',
+  headers: { 'Content-Type': 'application/json' },
 }
 
-const getByName = async (name: String, callback: Function) => {
-  return fetch(`${url}/breeds/name/${name}`)
+const getBreeds = async (callback: Function) => {
+ return fetch(`${url}/graphql`, {
+   ...options,
+  body: JSON.stringify({ query: '{ allBreeds { name, origin, description, temperament } }' }),
+  })
    .then((res) => res.json())
-   .then((data) => callback(data))
+   .then(({ data }) => callback(data.allBreeds))
+}
+
+
+const getByName = async (name: String, callback: Function) => {
+  return fetch(`${url}/graphql`, {
+    ...options,
+    body: JSON.stringify({ query: `{ breedByName(name: "${name}") { name, origin, description, temperament } }` }),
+    })
+    .then(res => {
+      console.log(res);
+      return res;
+    })
+    .then((res) => res.json())
+    .then(({data}) => callback(data.breedByName))
 }
 
 export {
